3d12fb5623856ddf8827862611fc53af8e457068
Abhinaw Anand <116647367+Abhinaw-47@users.noreply.github.com>
2024-07-13 17:31:31 +0530

    Update global.js

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://localhost:3001";
+const BACKEND_URL = "https://mern-stack-to-dobackend-abhinaw-anands-projects.vercel.app";
 // const BACKEND_URL = "https://gettowork-backend.vercel.app";
 
 export default BACKEND_URL;
